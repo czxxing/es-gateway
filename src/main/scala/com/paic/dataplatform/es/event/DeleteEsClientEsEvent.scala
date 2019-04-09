f3808e97969271f43644a5ec99230d6ab490a3f1
czxxing <32296429@qq.com>
2019-04-09 18:59:21 +0800

    删除文件

--- src/main/scala/com/paic/dataplatform/es/event/DeleteEsClientEsEvent.scala
+++
@@ -1,8 +0,0 @@
-package com.paic.dataplatform.es.event
-
-/**
-  * Created by czx on 4/2/19.
-  */
-case class DeleteEsClientEsEvent(override val uuid:String, key:String,override val creatTime:String) extends EsEvent(uuid,creatTime ) {
-
-}
